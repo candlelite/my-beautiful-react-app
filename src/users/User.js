0a3aaf10f2969b2f77883ffe21f1480556d3dd58
= <=>
2021-01-18 12:45:04 +0100

    Tests sur les components

--- src/users/User.js
+++ src/users/User.js
@@ -1,9 +1,12 @@
 import React from 'react'
 
 const User = (props) => {
+  let age = ''
+  props.age ? age=props.age : age='NA'
+
   return (
     <div>
-      { props.children } | Name : { props.name } | age : { props.age }
+      { props.children } | Name : { props.name } | age : { age }
     </div>
   )
 }
--- src/users/Users.js
+++ src/users/Users.js
@@ -3,13 +3,27 @@ import User from './User'
 
 class Users extends Component {
   render() {
-    return (
-      <div>
+    if(this.props.groupe==='Fedora') {
+      return (
+        <div>
+          <h1>{ this.props.children } </h1>
+          <h2>{ this.props.groupe} </h2>
+          <User name='Alain' age='20'> enfant 1 </User>
+          <User name='Jean' age='30'>enfant 2 </User>
+          <User name='Roger'>enfant 2 </User>
+        </div>
+      )
+    } else if (this.props.groupe==='CentOS') {
+      return (
+        <div>
         <h1>{ this.props.children } </h1>
-        <User name='Alain' age='20'> enfant 1 </User>
-        <User name='Jean' age='30'>enfant 2 </User>
+        <h2>{ this.props.groupe} </h2>
+        <User name='Paul' age='20'> enfant 1 </User>
+        <User name='Pierre' age='30'>enfant 2 </User>
+        <User name='Jacques'>enfant 2 </User>
       </div>
     )
+    }
   }
 }
 
