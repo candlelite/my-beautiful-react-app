30ca1ecaa3b61a02a371f946ead74db4091fbfaa
candlelite <cat_bear75@yahoo.fr>
2021-01-19 20:59:11 +0100

    Events And Two-way Data Binding

--- src/App.js
+++ src/App.js
@@ -1,11 +1,38 @@
+import React, { Component } from 'react'
 import Users from './users/Users'
 import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <Users groupe='Fedora'>Users List</Users>
-    </div>
-  );
+class App extends Component {
+
+  state = {
+    name: "Fred"
+  }
+
+  changeName = (newName) => {
+    this.setState ({
+      name: newName
+    })
+  }
+
+  changeNameFromInput = (event) => {
+    this.setState ({
+      name: event.target.value
+    })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <br/><br/>
+        <Users groupe='Fedora'>Users List</Users>
+        <br/><br/>
+        <button onClick={() => this.changeName("Super Fred :(")}>Change name using annon function</button>
+        <button onClick={this.changeName.bind(this, "Super Fred :)")}>Change name using bien function</button>
+        <br/><br/>
+        <input type='text' onChange={this.changeNameFromInput} value={this.state.name}></input>
+        <h3>{ this.state.name }</h3>
+      </div>
+    );
+  }
 }
 export default App;
